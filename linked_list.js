58550e49faf15c0af7eb11289b6d0e98635cad62
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-04 23:43:16 +0800

    2 possible scripts for this project

---
+++ linked_list.js
@@ -0,0 +1,11 @@
+/* linked_list.js */
+
+/* class definition for a node point */
+class Node {
+  constructor() {}
+}
+
+/* class definition for a linked list */
+class LinkedList {
+  constructor() {}
+}
---
+++ linked_list.test.js
@@ -0,0 +1,1 @@
+/* linked_list.test.js */
